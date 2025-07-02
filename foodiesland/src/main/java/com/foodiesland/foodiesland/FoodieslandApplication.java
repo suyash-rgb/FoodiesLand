6ad61682e6c103a7be9b11a9c2764312aa210475
Suyash Baoney <suyashbaoney58@gmail.com>
2025-07-02 18:24:52 +0530

    SpringBoot Project setup with web, mongodb, validation, devtools & lombok dependencies

---
+++ foodiesland/src/main/java/com/foodiesland/foodiesland/FoodieslandApplication.java
@@ -0,0 +1,13 @@
+package com.foodiesland.foodiesland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieslandApplication.class, args);
+	}
+
+}
---
+++ foodiesland/src/test/java/com/foodiesland/foodiesland/FoodieslandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodiesland.foodiesland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieslandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
